bb3558e8b9f436ce0d367d1aa58d8ad374197a25
Dayyan Muhammad <m.dayyan367@gmail.com>
2022-09-22 20:48:26 -0600

    too late :'(

--- get.py
+++ get.py
@@ -1,2 +1,8 @@
 import requests
-print(requests.get("http://www.google.com/"))
+
+# print(requests.__version__)
+
+# print(requests.get("http://google.com"))
+
+response = requests.get("https://raw.githubusercontent.com/DaY-w/CMPUT-404-lab-1/master/get.py")
+print(response.content)
